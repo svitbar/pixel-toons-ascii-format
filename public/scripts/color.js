db2f3d79b80c6701f0690e61886667c2689154e8
MytsV <mytsvictoria@gmail.com>
2022-04-23 03:00:00 +0300

    Implemented fromHex() function
    Co-authored-by: Daria Petruk <90788327+Mikkador@users.noreply.github.com>
    Co-authored-by: svitbar <89994710+svitbar@users.noreply.github.com>
    Co-authored-by: neliudochka <90915275+neliudochka@users.noreply.github.com>
    Co-authored-by: Mahaon17 <89358047+Mahaon17@users.noreply.github.com>

--- public/scripts/color.js
+++ public/scripts/color.js
@@ -13,6 +13,12 @@ export class Color {
 
   //"Factory method" that creates color from given HEX value
   static fromHex(hexColor) {
+    const radix = 16;
+
+    const r = parseInt(hexColor.slice(1, 3), radix);
+    const g = parseInt(hexColor.slice(3, 5), radix);
+    const b = parseInt(hexColor.slice(5, 7), radix);
+    return new Color(r, g, b);
   }
 
   //Converts color to RGBA CSS format. Use when passing color to CSS style parameter
